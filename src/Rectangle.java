ac0c6273a8346c3b97b19d25624b5a8d0238c0c4
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-25 21:32:33 +0100

    add toString()

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,3 +1,32 @@
 public class Rectangle {
+  private double length;
+  private double width;
 
+  public Rectangle(double length, double width) {
+    this.length = length;
+    this.width = width;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("length=%.2f, width=%.2f, square=%.2f",
+        length, width, length * width);
+
+  }
+
+  public double getLength() {
+    return length;
+  }
+
+  public double getWidth() {
+    return width;
+  }
+
+  public void setLength(double length) {
+    this.length = length;
+  }
+
+  public void setWidth(double width) {
+    this.width = width;
+  }
 }
