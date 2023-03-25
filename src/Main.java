69f7ea4f3521afe65e3ab3f0febf33d2846fcf7c
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-25 21:33:06 +0100

    add Main with test code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Rectangle rectangle1 = new Rectangle(2, 4);
+    Rectangle rectangle2 = new Rectangle(4, 2);
+    Rectangle rectangle3 = new Rectangle(4, 6);
+    Rectangle rectangle4 = new Rectangle(6, 8);
+    List<Rectangle> listRect = new ArrayList<>();
+    listRect.add(rectangle1);
+    listRect.add(rectangle2);
+    listRect.add(rectangle3);
+    listRect.add(rectangle4);
+    listRect.sort(new RectangleComparator());
+    for (Rectangle rectangle : listRect) {
+      System.out.println(rectangle);
+    }
+
   }
 }
